8bd387305bf565d23385f9f93efbbb9a951c8dca
Vitor Hugo <garotao102009@gmail.com>
2025-01-17 07:05:55 -0300

    Tabela Banco Dados

---
+++ Locação/Controllers/LocacaoController.cs
@@ -0,0 +1,22 @@
+﻿using Locação.Data;
+using Locação.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Locacao.Controllers
+{
+	public class LocacaoController : Controller
+	{
+		readonly private ApplicationDbContext _db;
+
+		public LocacaoController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<LocacaoModel> locacao = _db.Locacao;
+			return View(locacao);
+		}
+	}
+}
